4f57028e19f709ab61732ba9c0c96236df0a0245
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-01 18:54:23 +0530

    Fixed issue of empty result on empty searchstring in ProposalSearchSpecification

--- Extremis.Infrastructure/Specifications/ProposalSearchSpecification.cs
+++ Extremis.Infrastructure/Specifications/ProposalSearchSpecification.cs
@@ -7,13 +7,13 @@ public class ProposalSearchSpecification : BaseSpecification<Proposal>
 {
     public ProposalSearchSpecification(string searchString)
     {
-        searchString = searchString.ToLower();
-        if (string.IsNullOrWhiteSpace(searchString))
+        if (string.IsNullOrEmpty(searchString))
         {
             FilterCondition = p => true;
         }
         else
         {
+            searchString = searchString.ToLower();
             FilterCondition = p =>
                 p.Title.ToLower().Contains(searchString) || p.Description.ToLower().Contains((searchString));
         }
