b5591d7406ae4909b8e198548be539d1e1d89c53
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 05:21:11 +0530

    Added new endpoints in ProposalController

--- Extremis.Server/Controllers/ProposalController.cs
+++ Extremis.Server/Controllers/ProposalController.cs
@@ -15,8 +15,9 @@ public class ProposalController : ControllerBase
         string orderBy = null)
     {
         var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
-        return Ok(await _proposalService.GetAllMyProposals(pageNumber, pageSize, userId));
+        return Ok(await _proposalService.GetAllMyProposals(pageNumber, pageSize, searchString, userId));
     }
+    
     [HttpGet]
     public async Task<IActionResult> GetAllProposals(int pageNumber, int pageSize, string searchString,
         string orderBy = null)
@@ -37,5 +38,21 @@ public class ProposalController : ControllerBase
         var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
         var userName = HttpContext.User.FindFirstValue(JwtClaimTypes.PreferredUserName);
         return Ok(await _proposalService.CreateProposal(request, userName, userId));
+    }
+
+    [HttpPost("apply")]
+    public async Task<IActionResult> ApplyForProposal(ApplyForProposalRequestDto request)
+    {
+        var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
+        var userName = HttpContext.User.FindFirstValue(JwtClaimTypes.PreferredUserName);
+        return Ok(await _proposalService.ApplyForProposal(request, userName, userId));
+    }
+    
+    [HttpGet("candidates")]
+    public async Task<IActionResult> GetAllCandidates(int pageNumber, int pageSize, string searchString,
+        string id)
+    {
+        var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
+        return Ok(await _proposalService.GetAllCandidates(pageNumber, pageSize, searchString, id, userId));
     }
 }
