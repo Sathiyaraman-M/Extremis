188b56ad593d4bd0202dd0a120bc2f138bb7fc51
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 05:21:49 +0530

    Add new methods in client-side ProposalService for new endpoints

--- Extremis.Client.Web/Services/ProposalService.cs
+++ Extremis.Client.Web/Services/ProposalService.cs
@@ -22,10 +22,31 @@ public class ProposalService
     {
         return await _httpClient.GetFromJsonAsync<PaginatedResult<ProposalDto>>($"api/proposals?pageNumber={pageNumber}&pageSize={pageSize}&searchString={searchString}");
     }
+
+    public async Task<IResult<ProposalDto>> GetProposal(string id)
+    {
+        return await _httpClient.GetFromJsonAsync<Result<ProposalDto>>($"api/proposals/{id}");
+    }
+
+    public async Task<IResult<ProposalDto>> GetProposal(Guid id)
+    {
+        return await _httpClient.GetFromJsonAsync<Result<ProposalDto>>($"api/proposals/{id}");
+    }
     
     public async Task<IResult> CreateProposal(CreateProposalRequestDto request)
     {
         var response = await _httpClient.PostAsJsonAsync("api/proposals/create", request);
         return await response.ToResult();
+    }
+    
+    public async Task<IResult> ApplyForProposal(ApplyForProposalRequestDto request)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/proposals/apply", request);
+        return await response.ToResult();
+    }
+
+    public async Task<PaginatedResult<ReciprocatorDto>> GetAllCandidates(int pageNumber, int pageSize, string searchString, string id)
+    {
+        return await _httpClient.GetFromJsonAsync<PaginatedResult<ReciprocatorDto>>($"api/proposals/candidates?pageNumber={pageNumber}&pageSize={pageSize}&searchString={searchString}&id={id}");
     }
 }
