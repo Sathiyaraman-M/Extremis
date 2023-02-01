582ca33439a2947d2cdc56308660a31b52ff6395
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 05:20:30 +0530

    Create ReciprocatorDto

---
+++ Extremis.Application.Contracts/Proposals/ReciprocatorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Extremis.Proposals;
+
+public class ReciprocatorDto
+{
+    public string Id { get; set; }
+    public string ReciprocatorId { get; set; }
+    public string ProposalId { get; set; }
+    public string Note { get; set; }
+
+    public string CreatedBy { get; set; }
+    public string CreatedByUserId { get; set; }
+    public DateTime CreatedOn { get; set; }
+}
