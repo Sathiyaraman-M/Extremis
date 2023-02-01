f7b164e20ff4eee68caea2ef387f27e932e161a7
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 05:20:16 +0530

    Create ReciprocationSearchSpecification

---
+++ Extremis.Infrastructure/Specifications/ReciprocationSearchSpecification.cs
@@ -0,0 +1,21 @@
+﻿using Extremis.Proposals;
+using Extremis.Specifications.Base;
+
+namespace Extremis.Specifications;
+
+public class ReciprocationSearchSpecification : BaseSpecification<Reciprocation>
+{
+    public ReciprocationSearchSpecification(string searchString)
+    {
+        if (string.IsNullOrEmpty(searchString))
+        {
+            FilterCondition = p => true;
+        }
+        else
+        {
+            searchString = searchString.ToLower();
+            FilterCondition = p =>
+                p.Proposal.Title.ToLower().Contains(searchString) || p.Proposal.Description.ToLower().Contains((searchString));
+        }
+    }
+}
