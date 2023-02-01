91e748734d15f282902f0b36f95457d10cc49b92
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-02-01 11:55:09 +0530

    Created client-side ProposalService

---
+++ Extremis.Client.Web/Services/ProposalService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+using Extremis.Client.Extensions;
+using Extremis.Proposals;
+
+namespace Extremis.Client.Services;
+
+public class ProposalService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProposalService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<PaginatedResult<ProposalDto>> GetAllMyProposals(int pageNumber, int pageSize, string searchString)
+    {
+        return await _httpClient.GetFromJsonAsync<PaginatedResult<ProposalDto>>("api/proposals/mine");
+    }
+
+    public async Task<PaginatedResult<ProposalDto>> GetAllProposals(int pageNumber, int pageSize, string searchString)
+    {
+        return await _httpClient.GetFromJsonAsync<PaginatedResult<ProposalDto>>("api/proposals");
+    }
+    
+    public async Task<IResult> CreateProposal(CreateProposalRequestDto request)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/proposals/create", request);
+        return await response.ToResult();
+    }
+}
