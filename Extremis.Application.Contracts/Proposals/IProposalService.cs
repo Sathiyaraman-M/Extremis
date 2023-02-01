6dce8975999bf3ea01d3af5427c6cecba5b1ad28
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-02-01 09:54:14 +0530

    Added GetAllProposals method in ProposalService

--- Extremis.Application.Contracts/Proposals/IProposalService.cs
+++ Extremis.Application.Contracts/Proposals/IProposalService.cs
@@ -4,6 +4,7 @@ namespace Extremis.Proposals;
 
 public interface IProposalService
 {
+     Task<PaginatedResult<ProposalDto>> GetAllProposals(int pageNumber, int pageSize, string searchString, string orderBy);
      Task<IResult<ProposalDto>> GetProposal(string id);
-     Task<IResult> CreateProposal(CreateProposalRequestDto request);
+     Task<IResult> CreateProposal(CreateProposalRequestDto request, string userName, string userId);
 }
--- Extremis.Application/Proposals/ProposalService.cs
+++ Extremis.Application/Proposals/ProposalService.cs
@@ -1,5 +1,8 @@
-﻿using Extremis.Repositories;
+﻿using System.Linq.Expressions;
+using Extremis.Extensions;
+using Extremis.Repositories;
 using Extremis.Wrapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace Extremis.Proposals;
 
@@ -12,13 +15,90 @@ public class ProposalService : IProposalService
         _unitOfWork = unitOfWork;
     }
 
-    public Task<IResult<ProposalDto>> GetProposal(string id)
+    public async Task<PaginatedResult<ProposalDto>> GetAllProposals(int pageNumber, int pageSize, string searchString, string orderBy)
     {
-        throw new NotImplementedException();
+        try
+        {
+            Expression<Func<Proposal, ProposalDto>> expression = proposal => new ProposalDto()
+            {
+                CreatedOn = proposal.CreatedOn,
+                Description = proposal.Description,
+                Duration = proposal.Duration,
+                ExpireTime = proposal.ExpireTime,
+                Id = proposal.Id,
+                LastModifiedOn = proposal.LastModifiedOn,
+                ProposerId = proposal.ProposerId,
+                ProposerName = proposal.Proposer.FullName,
+                Status = proposal.Status,
+                Title = proposal.Title
+            };
+            return await _unitOfWork.GetRepository<Proposal>().Entities
+                .Include(x => x.Proposer)
+                .OrderByDescending(x => x.CreatedOn)
+                .ThenByDescending(x => x.LastModifiedOn)
+                .Select(expression)
+                .ToPaginatedListAsync(pageNumber, pageSize);
+        }
+        catch (Exception e)
+        {
+            return PaginatedResult<ProposalDto>.Failure(new List<string>() { e.Message });
+        }
     }
 
-    public Task<IResult> CreateProposal(CreateProposalRequestDto request)
+    public async Task<IResult<ProposalDto>> GetProposal(string id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var proposal = await _unitOfWork.GetRepository<Proposal>().Entities
+                .Include(x => x.Proposer)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            var proposalDto = new ProposalDto()
+            {
+                CreatedOn = proposal.CreatedOn,
+                Description = proposal.Description,
+                ProposerId = proposal.ProposerId,
+                ProposerName = proposal.Proposer.FullName,
+                Status = proposal.Status,
+                Title = proposal.Title,
+                ExpireTime = proposal.ExpireTime,
+                Duration = proposal.Duration,
+                LastModifiedOn = proposal.LastModifiedOn,
+                Id = proposal.Id
+            };
+            return await Result<ProposalDto>.SuccessAsync(proposalDto);
+        }
+        catch (Exception e)
+        {
+            return await Result<ProposalDto>.FailAsync(e.Message);
+        }
+    }
+
+    public async Task<IResult> CreateProposal(CreateProposalRequestDto request, string userName, string userId)
+    {
+        try
+        {
+            var proposal = new Proposal()
+            {
+                CreatedBy = userName,
+                CreatedOn = DateTime.Now,
+                CreatedByUserId = userId,
+                LastModifiedBy = userName,
+                LastModifiedOn = DateTime.Now,
+                LastModifiedByUserId = userId,
+                Description = request.Description,
+                Duration = request.Duration,
+                ExpireTime = request.ExpireTime,
+                Title = request.Title,
+                ProposerId = userId,
+                Status = ProposalStatus.Open
+            };
+            await _unitOfWork.GetRepository<Proposal>().AddAsync(proposal);
+            await _unitOfWork.Commit();
+            return await Result.SuccessAsync("Created proposal successfully!");
+        }
+        catch (Exception e)
+        {
+            return await Result.FailAsync(e.Message);
+        }
     }
 }
