2bb8c707cf0c180fd05429b5b503dd9c07d6cf5b
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-02-01 09:36:13 +0530

    Added new extension methods

---
+++ Extremis.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using Extremis.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Extremis.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize) where T : class
+    {
+        if (source == null) throw new Exception("IQueryable source for pagination is empty.");
+        pageNumber = pageNumber == 0 ? 1 : pageNumber;
+        pageSize = pageSize == 0 ? 10 : pageSize;
+        var count = await source.CountAsync();
+        pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
+    }
+    
+    public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> query, bool condition, Expression<Func<TSource, bool>> predicate)
+    {
+        return condition ? query.Where(predicate) : query;
+    }
+}
---
+++ Extremis.Infrastructure/Extensions/SpecificationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Extremis.Contracts;
+using Extremis.Specifications.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Extremis.Extensions;
+
+public static class SpecificationExtensions
+{
+    public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> spec) where T : class, IEntity
+    {
+        if (spec == null) 
+            return query;
+        
+        if (spec.FilterCondition != null)
+            return query.Where(spec.FilterCondition);
+
+        query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+        query = spec.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
+
+        return query;
+    }
+    
+    public static ISpecification<T> And<T>(this ISpecification<T> left, ISpecification<T> right) where T : class, IEntity
+    {
+        return new AndSpecification<T>(left, right);
+    }
+
+    public static ISpecification<T> Or<T>(this ISpecification<T> left, ISpecification<T> right) where T : class, IEntity
+    {
+        return new OrSpecification<T>(left, right);
+    }
+}
