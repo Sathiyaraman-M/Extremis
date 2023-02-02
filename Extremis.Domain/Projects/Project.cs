e79a18bffe44ccbcc6119898279ce47a178e0d73
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-02-02 13:49:50 +0530

    Create Project and ProjectMember entities

---
+++ Extremis.Domain/Projects/Project.cs
@@ -0,0 +1,24 @@
+﻿using Extremis.Contracts;
+using Extremis.Users;
+
+namespace Extremis.Projects;
+
+public class Project : IAuditableEntity<string>
+{
+    public string Id { get; set; }
+    
+    public AppUser Owner { get; set; }
+    public string OwnerId { get; set; }
+    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    
+    public List<ProjectMember> Members { get; set; }
+
+    public string CreatedBy { get; set; }
+    public string CreatedByUserId { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string LastModifiedBy { get; set; }
+    public string LastModifiedByUserId { get; set; }
+    public DateTime? LastModifiedOn { get; set; }
+}
---
+++ Extremis.Domain/Projects/ProjectMember.cs
@@ -0,0 +1,15 @@
+﻿using Extremis.Contracts;
+using Extremis.Users;
+
+namespace Extremis.Projects;
+
+public class ProjectMember : IEntity<string>
+{
+    public string Id { get; set; }
+    
+    public Project Project { get; set; }
+    public string ProjectId { get; set; }
+    
+    public AppUser Member { get; set; }
+    public string MemberId { get; set; }
+}
