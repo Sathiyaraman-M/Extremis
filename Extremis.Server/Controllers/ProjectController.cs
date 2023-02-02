18093027d3023d75afbc507cacee57f4063cbaeb
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 18:34:54 +0530

    Update ProjectController

--- Extremis.Server/Controllers/ProjectController.cs
+++ Extremis.Server/Controllers/ProjectController.cs
@@ -8,13 +8,24 @@ public class ProjectController : Controller
 {
     private readonly IProjectService _projectService;
 
-    public ProjectController(IProjectService projectService)
+    public ProjectController(IProjectService projectService) => _projectService = projectService;
+
+    [HttpGet("own")]
+    public async Task<IActionResult> GetAllMyProjects(int pageNumber, int pageSize)
     {
-        _projectService = projectService;
+        var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
+        return Ok(await _projectService.GetAllMyProjects(pageNumber, pageSize, userId));
+    }
+
+    [HttpGet("joined")]
+    public async Task<IActionResult> GetAllJoinedProjects(int pageNumber, int pageSize)
+    {
+        var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
+        return Ok(await _projectService.GetAllJoinedProjects(pageNumber, pageSize, userId));
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateProject(CreateProjectDto request)
+    public async Task<IActionResult> CreateProject(CreateProjectRequestDto request)
     {
         var userId = HttpContext.User.FindFirstValue(JwtClaimTypes.Subject);
         var userName = HttpContext.User.FindFirstValue(JwtClaimTypes.PreferredUserName);
