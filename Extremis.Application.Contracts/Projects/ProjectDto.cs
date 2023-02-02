42b85ddf652b08ca7242f796f46a686e13637c9c
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 18:34:30 +0530

    Create ProjectDto

---
+++ Extremis.Application.Contracts/Projects/ProjectDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Extremis.Projects;
+
+public class ProjectDto
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string OwnerName { get; set; }
+    public int MembersCount { get; set; }
+}
