0fd26dcfae6c611fe61bb54afb7896caab6258c9
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-02 20:57:24 +0530

    Create ProjectFullDto and ProjectMemberDto

---
+++ Extremis.Application.Contracts/Projects/ProjectFullDto.cs
@@ -0,0 +1,17 @@
+﻿using Extremis.Users;
+
+namespace Extremis.Projects;
+
+public class ProjectFullDto
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string OwnerId { get; set; }
+    public string OwnerName { get; set; }
+    
+    public UserStatus OwnerStatus { get; set; }
+    
+    public string OwnerCustomStatus { get; set; }
+    public List<ProjectMemberDto> Members { get; set; } = new();
+}
---
+++ Extremis.Application.Contracts/Projects/ProjectMemberDto.cs
@@ -0,0 +1,12 @@
+﻿using Extremis.Users;
+
+namespace Extremis.Projects;
+
+public class ProjectMemberDto
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public UserStatus Status { get; set; }
+    public string CustomStatus { get; set; }
+}
